8945596a44f5f9948797d36eb004bd8db761e480
gaorw <liulangreng7@sina.com>
2015-08-09 03:50:04 +0800

    修改了代码
    Change-Id: I1d8625144f450dcd62bd6756e75bf13a67ae0b7a

--- src/git/FGitTest.java
+++ src/git/FGitTest.java
@@ -7,6 +7,7 @@ public class FGitTest {
 
   public static void main(String[] args) {
     System.out.println("this is my first git manager porject!");
+    System.out.println("I want to modify the code!");
   }
 
 }
