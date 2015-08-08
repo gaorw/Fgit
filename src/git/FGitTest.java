8b0024f47f5511da132cbdbc060546fc509c97bf
gaorw <liulangreng7@sina.com>
2015-08-09 02:23:11 +0800

    首次自己配置git提交代码
    Change-Id: I8e5c9255e8fe06c1c54ba912daf70a4e083f9d31

---
+++ src/git/FGitTest.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * 第一个Git工程及Github代码管理
+ */
+public class FGitTest {
+
+  public static void main(String[] args) {
+    System.out.println("this is my first git manager porject!");
+  }
+
+}
